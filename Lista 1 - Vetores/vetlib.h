34961877218a1e552ae2e52e443b188c003f882a
LuizHenriqueSilverio <luizhenriquesilverios@hotmail.com>
2022-08-11 09:58:07 -0300

    Inicio de refatoramento de exercícios.

--- Lista 1 - Vetores/vetlib.h
+++ Lista 1 - Vetores/vetlib.h
@@ -24,3 +24,13 @@ void printaVetor(int v[], int n){
 }
 
 //-------------------------------------
+
+void somaVetor(int v[], int n){
+	int soma = 0;
+	
+	for(int i = 0; i < n; i++){
+		soma += v[i];
+	}
+	
+	return soma;
+}
