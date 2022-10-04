393c088c7e56b7524ded956c4c48f9fe446873ef
LuizHenriqueSilverio <luizhenriquesilverios@hotmail.com>
2022-10-04 20:20:28 -0300

    Struct finalizada.

--- Exercício_Structs/StructConcessionaria.c
+++ Exercício_Structs/StructConcessionaria.c
@@ -93,6 +93,90 @@ int filtraMarca(tveiculo v[], char marcaConsulta[40]){
 
 //--------------------------------------------
 
+int selecionaVeiculo(tveiculo v[], int n){
+	if(n > qtd + 1){
+		printf("Não existe veiculo nesse indice!");
+		return -1;
+	}else{
+		printf("*** Veiculo %d ***\n", n + 1);
+		printf("Modelo: %s\n", v[n].modelo);
+		printf("Marca: %s\n", v[n].marca);
+		printf("Ano de fabricacao: %d\n", v[n].ano);
+		printf("Preco de venda: %.2f\n", v[n].preco);
+		printf("------------------------\n");
+		return n;
+	}
+}
+
+//--------------------------------------------
+
+void alteraVeiculo(tveiculo v[], int n){
+	
+	printf("Nome do modelo: ");
+	fflush(stdin);
+	gets(v[n].modelo);
+	
+	printf("Nome da marca: ");
+	fflush(stdin);
+	gets(v[n].marca);
+	
+	printf("Ano de fabricacao: ");
+	fflush(stdin);
+	scanf("%d", &v[n].ano);
+	
+	printf("Valor de venda: ");
+	fflush(stdin);
+	scanf("%f", &v[n].preco);
+	
+}
+
+//--------------------------------------------
+
+void removeVeiculo(tveiculo v[], int n){
+	int pos, op;
+	
+	pos = selecionaVeiculo(v, n);
+	if(pos == -1){
+		printf("\nVeiculo nao encontrado!\n");
+	}
+	printf("\nTem certeza que deseja remover o veiculo? [1-Sim | 2-Nao]\n Opcao: ");
+	scanf("%d", &op);
+	if(op == 1){
+		v[pos] = v[qtd - 1];
+		qtd--;
+		printf("Veiculo removido com sucesso!");
+	}else{
+		printf("Operacao cancelada!");
+	}
+}
+
+//--------------------------------------------
+
+void salvaArquivo(tveiculo veiculos[]){
+	FILE *arq;
+	int i = 0;
+	arq = fopen("dadosVeiculos.txt", "wb");
+	fwrite(&veiculos[i], sizeof(tveiculo), qtd, arq);
+	printf("Dados salvos com sucesso.\n");
+	fclose(arq);
+}
+
+//--------------------------------------------
+
+void carregaArquivo(tveiculo veiculos[]){
+	FILE *arq;
+	arq = fopen("dadosVeiculos.txt", "rb");
+	if(arq == NULL){
+		printf("Arquivo de veiculos nao encontrado!\n");
+		return;
+	}
+	while(fread(&veiculos[qtd], sizeof(tveiculo), 1, arq) > 0)
+		qtd++;
+	printf("Dados carregado com sucesso!\n");
+	fclose(arq);
+}
+
+//--------------------------------------------
 int menu(){
 	int opcao;
 	
@@ -100,7 +184,9 @@ int menu(){
 	printf("1 - Cadastrar veiculo\n");
 	printf("2 - Listar veiculos\n");
 	printf("3 - Filtrar por preco\n");
-	printf("4 - FIltrar por marca\n");
+	printf("4 - Filtrar por marca\n");
+	printf("5 - Alterar informacoes de um veiculo\n");
+	printf("6 - Remover um veiculo\n");
 	printf("0 - Sair\n");
 	printf("Opcao: ");
 	scanf("%d", &opcao);
@@ -113,9 +199,10 @@ int menu(){
 void main(){
 	
 	tveiculo veiculos[100];
-	int op, busca, res;
+	int op, res, busca;
 	float precoMin, precoMax;
 	char marcaBusca[40], modeloBusca[40];
+	carregaArquivo(veiculos);
 	
 	do{
 		op = menu();
@@ -146,8 +233,20 @@ void main(){
 					printf("Nao ha veiculos dessa marca! \n");
 				}
 				break;
+			case 5:
+				listaVeiculos(veiculos);
+				printf("Insira o codigo do veiculo para alteracao: ");
+				scanf("%d", &busca);
+				alteraVeiculo(veiculos, (busca - 1));
+				break;
+			case 6:
+				printf("Codigo do veiculo: ");
+				scanf("%d", &busca);
+				removeVeiculo(veiculos, (busca - 1));
+				break;
 			case 0:
 				printf("Saindo...\n");
+				salvaArquivo(veiculos);
 				break;
 			default:
 				printf("Opcao invalida!!!\n");
